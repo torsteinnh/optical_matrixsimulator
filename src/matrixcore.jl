9bdaa842e169bec25f286929017f5880efcf8941
torsteinnh <torsteinnh@gmail.com>
2021-08-29 20:05:42 +0200

    Simple transfer and scattering matrix system

---
+++ src/matrixcore.jl
@@ -0,0 +1,56 @@
+module matrixcore
+
+export StoM, MtoS, CascadeScattering
+
+using LinearAlgebra
+
+
+function StoM(S::Matrix{T})::Matrix{<:Number} where T <: Number
+    # Converts a 2x2 scattering matrix to a transfer matrix
+    # See Saleh & Teich 3.ed eq.7.1-6
+    @assert(size(S) == (2, 2))
+    @inbounds begin
+        t12 = S[1, 1]
+        r21 = S[1, 2]
+        r12 = S[2, 1]
+        t21 = S[2, 2]
+    end
+        M = [
+        t12*t21 - r12*r21 r21;
+        -r12              1
+    ]
+    (1/t21) .* M
+end
+
+function MtoS(M::Matrix{T})::Matrix{<:Number} where T <: Number
+    # Converts a 2x2 transfer matrix to a scattering matrix
+    # See Saleh & Teich 3.ed eq.7.1-5
+    @assert(size(M) == (2, 2))
+    @inbounds begin
+        A = M[1, 1]
+        B = M[1, 2]
+        C = M[2, 1]
+        D = M[2, 2]
+    end
+        S = [
+        A*D - B*C B;
+        -C        1
+    ]
+    (1/D) .* S
+end
+
+
+function CascadeScattering(layers::Vector{Matrix{T}}) where T <: Number
+    # Generates the total S matrix for a layered system
+    # Matrixes are given in the order the elements appear, the array is inversed inside the function
+    accumulated = I
+
+    for S in view(layers, length(layers):-1:1)
+        accumulated *= StoM(S)
+    end
+
+    MtoS(accumulated)
+end
+
+
+end # matrixcore
---
+++ test/test_matrixcore.jl
@@ -0,0 +1,51 @@
+using Test
+
+include("../src/matrixcore.jl")
+using .matrixcore
+
+
+@testset "test_matrixcore" begin
+    
+    @testset "StoMtoS" begin
+        # Running random tests to check that MtoS and StoM are each others inverse
+        for _ in 1:100
+            S = rand(ComplexF64, 2, 2)
+            @test S ≈ (StoM ∘ MtoS)(S)
+        end
+    end
+
+    @testset "test_cascades" begin
+        # Getting random transmittance and reflectances, testing two mirror systems
+        # Saleh & Teich 3.ed eq.7.1-8
+        for _ in 1:100
+            t12 = rand(ComplexF64)
+            t21 = rand(ComplexF64)
+
+            r12 = rand(ComplexF64)
+            r21 = rand(ComplexF64)
+
+            t23 = rand(ComplexF64)
+            t32 = rand(ComplexF64)
+
+            r23 = rand(ComplexF64)
+            r32 = rand(ComplexF64)
+
+            ϕ = rand() * 2π
+
+            mirror1 = [
+                t12 r21;
+                r12 t21
+            ]
+            air = [ℯ^(-ϕ * im) 0; 0 ℯ^(-ϕ * im)]
+            mirror2 = [
+                t23 r32;
+                r23 t32
+            ]
+
+            machine = CascadeScattering([mirror1, air, mirror2])
+
+            @test machine[1, 1] ≈ (t12 * t23 * ℯ^(-ϕ * im)) / (1 - (r21 * r23 * ℯ^(-ϕ * 2im)))
+            @test machine[2, 1] ≈ r12 + (t12 * t21 * r23 * ℯ^(-ϕ * 2im)) / (1 - (r21 * r23 * ℯ^(-ϕ * 2im)))
+        end
+    end
+end
